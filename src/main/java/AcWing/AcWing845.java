a440428bca5a6b5b8ad216ef5db089262883c1c2
xuchen <564974611@qq.com>
2021-07-04 22:02:47 +0800

    2021/07/04 java

--- src/main/java/AcWing/AcWing845.java
+++ src/main/java/AcWing/AcWing845.java
@@ -7,8 +7,8 @@ public class AcWing845 {
     private static final int MAX_N = 3;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String grid = scanner.nextLine();
-        grid = grid.split("  ");
+        String[] grid = scanner.nextLine().split("  ");
+        // grid = grid.split("  ");
         System.out.println(grid);
     }
 }
