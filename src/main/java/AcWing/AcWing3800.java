899d4f11b362ceecd7f92abb467a7ccb384fe73f
xuchen <564974611@qq.com>
2021-08-23 23:53:13 +0800

    2021/08/23

---
+++ src/main/java/AcWing/AcWing3800.java
@@ -0,0 +1,34 @@
+package AcWing;
+
+import java.util.Scanner;
+
+public class AcWing3800 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        while (T > 0) {
+            T--;
+            int b = scanner.nextInt();
+            int k = scanner.nextInt();
+            boolean numB = isEven(b);
+            int result = 0;
+            for (int i = 0; i < k; i++) {
+                int x = scanner.nextInt();
+                if (i != k - 1) {
+                    result = result + ((numB || isEven(x)) ? 0 : 1);
+                } else {
+                    result = result + (isEven(x) ? 0 : 1);
+                }
+            }
+            if ((result & 1) == 1) {
+                System.out.println("odd");
+            } else {
+                System.out.println("even");
+            }
+        }
+    }
+
+    private static boolean isEven(int x) {
+        return (x & 1) == 0;
+    }
+}
