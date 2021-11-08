8a38ce91a0313a4cde4668d091e44842b522a6a7
xuchen <564974611@qq.com>
2021-11-08 20:48:32 +0800

    2021/11/08

---
+++ src/main/java/projectEuler/Problem35.java
@@ -0,0 +1,62 @@
+package projectEuler;
+
+import java.util.*;
+
+public class Problem35 {
+    private static int MAX_N = 1000007;
+
+    private static boolean[] visited = new boolean[MAX_N];
+
+    private static int[] prime = new int[MAX_N];
+
+    public static void main(String[] args) {
+        int cnt = 0;
+        for (int i = 2; i < MAX_N; i++) {
+            if (!visited[i]) {
+                prime[cnt++] = i;
+            }
+            for (int k = 0; k < cnt && i * prime[k] < MAX_N; k++) {
+                visited[i * prime[k]] = true;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < cnt; i++) {
+            if (checkFunc(prime[i])) {
+                result++;
+            }
+        }
+
+        System.out.println("answer:" + result);
+    }
+
+    private static boolean checkFunc(int tempNum) {
+        List<Integer> nums = getRotationNum(tempNum);
+
+        for (int num : nums) {
+            if (visited[num]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static List<Integer> getRotationNum(int num) {
+        List<Integer> nums = new LinkedList<>();
+        int cnt = 0;
+        int divisor = 1;
+        while (divisor < num) {
+            cnt++;
+            divisor *= 10;
+        }
+        divisor /= 10;
+
+        for (int i = 0; i < cnt; i++) {
+            nums.add(num);
+            num = (num % divisor) * 10 + num / divisor;
+        }
+
+        return nums;
+    }
+}
