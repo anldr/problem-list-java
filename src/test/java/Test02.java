23dff766a7a85b4ccc5cf4493fa523e65268108a
anldr <laptop@redmi>
2024-06-01 23:07:35 +0800

    update CirQueue

--- src/test/java/Test02.java
+++ src/test/java/Test02.java
@@ -1,5 +1,6 @@
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import z_others.CirQueue;
 
 @Slf4j
 public class Test02 {
@@ -12,5 +13,17 @@ public class Test02 {
         log.info("{}", str1 == str2);
         log.info("{}", str2 == str3);
         log.info("{}", str1 == str3);
+
+        CirQueue queue = new CirQueue(3);
+
+        log.info("{}", queue.push(1));
+        log.info("{}", queue.push(2));
+        log.info("{}", queue.push(3));
+        log.info("{}", queue.push(4));
+        log.info("{}", queue.back());
+        log.info("{}", queue.is_full());
+        log.info("{}", queue.pop());
+        log.info("{}", queue.push(4));
+        log.info("{}", queue.back());
     }
 }
